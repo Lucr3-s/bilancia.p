abb5f1fb11b43dfbf6fe5974b8091a658855f05a
Daniele Molinari <37416120+TheRav3n@users.noreply.github.com>
2021-12-07 00:55:24 +0100

    Update WeightScale.ino

--- WeightScale.ino
+++ WeightScale.ino
@@ -1,11 +1,51 @@
+#include <stdlib.h>
 #include "HX711.h"
 
-const int dout = 2; const int sck = 3;
+const int HISTORY_SIZE = 10; // history size for calculating average
+const int TOLERANCE = 3; // 3g tolerance
 
-HX711 bilancia(2, 3);
+// global variables
+HX711 weightScale(2, 3);
+int weight = 0; // current weight
+int previousWeight = 0;
+int sensorValues[HISTORY_SIZE];
 
-int peso = 0; // zona di memorizzazione del peso corrente int pesoprec = 0; // zona di memorizzazione dell'ultimo peso esposto void setup() { Serial.begin(9600);
+void setup() {
+  for(int i = 0; i < HISTORY_SIZE; i++) {
+   sensorValues[i] = 0;
+  }
+  
+  Serial.begin(9600);
 
-Serial.println("calcolo della tara"); Serial.println("non porre alcun oggetto sulla bilancia....."); delay (100); bilancia.set_scale(-224.16); // <---- valore di scala, scale value --------
+  Serial.println("calcolo della tara");
+  Serial.println("non porre alcun oggetto sulla bilancia.....");
+  
+  weightScale.set_scale(-224.16); // <---- valore di scala, scale value --------
+  weightScale.tare(30); // il peso attuale e' considerato tara Serial.println("sistema pronto");
+}
 
-bilancia.tare(30); // il peso attuale e' considerato tara Serial.println("sistema pronto"); } void loop() { peso = bilancia.get_units(20); if (!(peso == pesoprec)) // se e' variato il peso { pesoprec = peso; // memorizza il peso corrente Serial.print("peso: "); Serial.print(peso); Serial.println("g"); } delay(200); }
+void loop() {
+  // shift sensor values
+  for (int i = 1; i < 5 - 1; i++) {
+    arr[i - 1] = arr[i];
+  }
+  
+  // retrieve new value
+  arr[5] = weightScale.get_units(20);
+  
+  // calculate new weight
+  let sum;
+  for(int i = 0; i < HISTORY_SIZE; i++){
+   sum += sensorValues[i];
+  }
+  previousWeight = weight;
+  weight = sum / HISTORY_SIZE;
+  
+  if (abs(previousWeight - weight) < TOLERANCE) { // se e' variato il peso
+    Serial.print("weight: ");
+    Serial.print(weight);
+    Serial.println("g");
+  }
+  
+  delay(200);
+}
